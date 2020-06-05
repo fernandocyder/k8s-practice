25bfa9a0a1fd4258bf5a5b0a15d913ab1f6c9547
Fernando Karnagi <fernando@cyder.com.sg>
2020-06-05 13:48:03 +0800

    fixed security protocol for LB

--- 03.kafka/build-docker.sh
+++ 03.kafka/build-docker.sh
@@ -1,3 +1,3 @@
-docker rmi -f fernandokarnagi/kafka-server:v10
-docker build -t fernandokarnagi/kafka-server:v10 .
-docker push fernandokarnagi/kafka-server:v10
+docker rmi -f fernandokarnagi/kafka-server:v11
+docker build -t fernandokarnagi/kafka-server:v11 .
+docker push fernandokarnagi/kafka-server:v11
